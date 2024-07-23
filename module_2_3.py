b60611d19d455365229cc3c8508ae004c8338d68
NikolayBar <112650071+NikolayBar@users.noreply.github.com>
2024-07-23 16:34:05 +0300

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,8 +1,8 @@
-my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+my_list = [42, 69, 0, 322, 13, 0, 99, 5, 9, 8, 7, 6, 5]
 
 indx = -1
 
-while indx < len(my_list):
+while indx < len(my_list)-1:
     indx += 1
     if my_list[indx] > 0:
         print(my_list[indx])
